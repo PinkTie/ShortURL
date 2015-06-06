831b8c26419b0772816033a66a5946ba4d651d12
Kevin Fairbanks <kevin@kevinfairbanks.com>
2015-06-06 01:50:29 -0400

    Initial Commit

---
+++ application/language/english/en_admin_lang.php
@@ -0,0 +1,21 @@
+// Common form elements
+$lang['common_form_elements_next'] = "Next...";
+$lang['common_form_elements_save'] = "Save...";
+$lang['common_form_elements_cancel'] = "Cancel";
+$lang['common_form_elements_go'] = "Go...";
+$lang['common_form_elements_go_back'] = "Go back";
+$lang['common_form_elements_submission_error'] = "There were errors with the form:";
+$lang['common_form_elements_success_notify'] = "Success:";
+$lang['common_form_elements_error_notify'] = "Error:";
+$lang['common_form_elements_actions'] = "Actions";
+$lang['common_form_elements_action_edit'] = "Edit";
+$lang['common_form_elements_action_delete'] = "Delete";
+$lang['common_form_elements_active'] = "Active";
+$lang['common_form_elements_inactive'] = "Inactive";
+$lang['common_form_elements_successful_change'] = "Your changes have been saved";
+$lang['common_form_elements_successful_delete'] = "The item has been deleted";
+$lang['common_form_elements_yes'] = "Yes";
+$lang['common_form_elements_no'] = "No";
+$lang['common_form_elements_to'] = "to";
+$lang['common_form_elements_from'] = "from";
+$lang['common_form_elements_history'] = "History";
